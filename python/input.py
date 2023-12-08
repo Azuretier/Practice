fcf8a2c72792f5750c2d22d79bd5cde93f72d63a
Azureit <d.f.18th@gmail.com>
2023-12-08 22:32:21 +0900

    "end"と入力されるまでコードをループするように変更

--- python/input.py
+++ python/input.py
@@ -1,12 +1,16 @@
-# 任意の整数を入力させる
-user_input = input("任意の整数を入力してください: ")
+while True:
+    # 任意の整数を入力させる
+    user_input = input("任意の整数を入力してください: ")
 
-try:
-    # 入力された整数をintに変換
-    number = int(user_input)
-    # 入力された整数を表示
-    print(f"入力された整数は {user_input} です。")
+    if user_input == "end":
+        break
 
-except ValueError:
-    # エラーハンドリング
-    print("有効な整数を入力してください。")
+    try:
+        # 入力された整数をintに変換
+        number = int(user_input)
+        # 入力された整数を表示
+        print(f"入力された整数は {user_input} です。")
+
+    except ValueError:
+        # エラーハンドリング
+        print("有効な整数を入力してください。もう一度試してください。")
