d31ee11818c511680dc2ff63f5df886a3cd70a49
Azureit <d.f.18th@gmail.com>
2023-12-08 22:25:41 +0900

    入力する数字を整数に指定

--- python/input.py
+++ python/input.py
@@ -1,12 +1,12 @@
-# 任意の数字を入力させる
-user_input = input("任意の数字を入力してください: ")
+# 任意の整数を入力させる
+user_input = input("任意の整数を入力してください: ")
 
 try:
-    # 入力された数字をfloatに変換
-    number = float(user_input)
-    # 入力された数字を表示
-    print(f"入力された数字は {number} です。")
+    # 入力された整数をintに変換
+    number = int(user_input)
+    # 入力された整数を表示
+    print(f"入力された整数は {user_input} です。")
 
 except ValueError:
     # エラーハンドリング
-    print("有効な数字を入力してください。")
+    print("有効な整数を入力してください。")
